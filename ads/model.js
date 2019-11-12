47aeeb288273a63e75dfff91cba0962cbf81aa65
luisanegri <luisaanegri@gmail.com>
2019-11-12 17:06:15 +0100

    added extra columns to ad model

--- ads/model.js
+++ ads/model.js
@@ -1,11 +1,13 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-const Ad = db.define('ad', {
+const Ad = db.define("ad", {
   title: Sequelize.STRING,
   description: Sequelize.STRING,
   picture_url: Sequelize.STRING,
-  price: Sequelize.INTEGER
+  price: Sequelize.INTEGER,
+  email: Sequelize.STRING,
+  phone_number: Sequelize.INTEGER
 });
 
 module.exports = Ad;
